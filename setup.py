872bca86a8844887a100b18246b91846856c2775
Ouadia <ouadia.lasfar@gmail.com>
2018-08-31 00:39:02 +0200

    Upgrade requests library

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 
 setup(
     name='onesky-python',
-    version='1.0.2',
+    version='1.0.3',
     description='python wrapper for the OneSky REST API',
     author='Jana Mobile',
     author_email='technology@jana.com',
     maintainer='Dan OBrien',
     maintainer_email='danob@jana.com',
     install_requires=[
-        'requests==2.3.0',
+        'requests==2.11.1',
         'termcolor==1.1.0'
     ],
     license='LICENSE.txt',
